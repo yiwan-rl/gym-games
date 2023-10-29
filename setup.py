6b7b47fa06f2ea0d0b71d66d4e9339d1a18b4b5e
yiwan-rl <wan6@ualberta.ca>
2023-10-29 11:38:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,15 +17,11 @@ setup(
   packages=find_packages(),
   python_requires='>=3.5',
   install_requires=[
-    'numpy==1.22.3',
-    'MinAtar==1.0.10',
-    'gym==0.23.1',
-    'setuptools>=41.0.1',
-    'pygame==1.9.6',
-    'ple==0.0.1'
-  ],
-  dependency_links=[
-    'git+https://github.com/kenjyoung/MinAtar@master#egg=MinAtar-1.0.10',
-    'git+https://github.com/ntasfi/PyGame-Learning-Environment@master#egg=ple-0.0.1'
+    'numpy',
+    'MinAtar',
+    'gymnasium',
+    'setuptools',
+    'pygame',
+    'ple'
   ]
 )
